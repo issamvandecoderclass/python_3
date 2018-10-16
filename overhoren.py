6ff728716ff743a0fe0f35aea0ba3d86f07a5df5
issamvandecoderclass <issam.buamran@coderclass.nl>
2018-10-16 15:10:34 +0200

    extra 0,5 seconden toegevoegd

--- overhoren.py
+++ overhoren.py
@@ -4,19 +4,20 @@ import sys
 
 def groeting():
     os.system("cls")
-    groeting.start = input("|a|: Nieuwe woordenlijst maken. \n |b|: woorden toe te voegen aan een woordenlijst. \n |h|: woordenlijst veranderen. \n |x|: woordelijst verwijderen. \n |s|: Overhoren. \n |q|: Stoppen met het programma. \n")
+    groeting.start = input("|a|: nieuwe woordenlijst maken. \n |b|: woorden toe te voegen aan een woordenlijst. \n |h|: woordenlijst veranderen. \n |x|: woordelijst verwijderen. \n |s|: Overhoren. \n |q|: Stoppen met het programma. \n")
 
 def nieuwe_lijst_naam():
     print("\n" * 11)
     naam_woordenlijst = input("Hoe wil je je woordenlijst noemen? ")
     bestaat_al = os.path.isfile(naam_woordenlijst)
     if bestaat_al:
+        print("\n" * 5)
         print("bestaat al")
     else:
         with open(naam_woordenlijst,"w+") as f:
             f.write("Bestand is aangemaakt")
-        print("Nieuwe woorden lijst: " + naam_woordenlijst + " is gemaakt.")
-    print("\n" * 5)
+        print("\n" * 5)
+        print("Nieuwe woorden lijst - " + naam_woordenlijst + " - is gemaakt.")
     main()
 
 def lijst_verwijderen():
@@ -26,7 +27,7 @@ def lijst_verwijderen():
     if bestaat_lijst_del:
         os.remove(del_lijst)
         print("\n" * 5)
-        print("Lijst " + del_lijst +  " is succesvol verwijdert.")
+        print("Lijst - " + del_lijst +  " - is succesvol verwijdert.")
         print("\n" * 1)
         main()
     else:
@@ -41,10 +42,13 @@ def overhoren():
     if bestaat_lijst_overhoor:
         print("Je kan worden overhoort.")
     else:
+        print("\n" * 5)
         print("Bestand kan niet worden overhoort.")
+        print("\n" * 1)
+        main()
 
 def geef_afscheid():
-    print("Dit programma sluit over 5 seconden")
+    print("Dit programma sluit over 5 seconden,")
     print("5")
     time.sleep(0.5)
     print("4")
@@ -55,11 +59,13 @@ def geef_afscheid():
     time.sleep(0.5)
     print("1")
     time.sleep(0.5)
+    print("0")
+    time.sleep(0.5)
     sys.exit()
 
 def voeg_woorden_toe():
     print("Voeg woorden toe")
-    lijst_keuze = input("Naar welke lijst wil je toe.")
+    lijst_keuze = input("Naar welke lijst wil je toe?")
     bestaat_lijst = os.path.isfile(lijst_keuze)
     if bestaat_lijst == True:
         print("bestaat")
